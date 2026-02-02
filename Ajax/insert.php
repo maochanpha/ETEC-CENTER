ad97022fdd5fb156c3e3a14d7dae250f65341136
maochanpha402-svg <maochanpha402@gmail.com>
2026-02-02 08:34:20 +0700

    update Ajax

--- Ajax/insert.php
+++ Ajax/insert.php
@@ -1,16 +1,18 @@
 <?php
-include 'db.php';
-if(isset($_POST['submit'])){
-    $id = $_POST['id'];
+require 'db.php';
+if ($_SERVER['REQUEST_METHOD']=='POST') {
+    if (!is_dir('profile')) {
+        mkdir('profile', 0777, true);
+    }
     $name = $_POST['name'];
     $gender = $_POST['gender'];
-    $profile = $_FILES['file']['name'];
+    $file = $_FILES['file']['name'];
     $tmp_name = $_FILES['file']['tmp_name'];
-    $path = "upload/".$profile;
+    $path = "upload/" .time(). '_' .$file;
     move_uploaded_file($tmp_name, $path);
-    $execute = "INSERT INTO tbl_student (name, gender, profile) VALUES ('$name', '$gender', '$path')";
-    mysqli_query($conn, $execute);
-    if($execute){
-        header("location: table.php");
+    $insert = "INSERT INTO tbl_student (name, gender, profile) VALUES ('$name', '$gender', '$path')";
+    $ex = $conn->query($insert);
+    if ($ex) {
+        echo "Success";
     }
 }
--- Ajax/table.php
+++ Ajax/table.php
@@ -13,6 +13,43 @@
 <body>
   <div class="container mt-5 p-4 rounded-3 shadow">
     <button type="button" id="add" class="btn btn-dark float-end mb-2" data-bs-toggle="modal" data-bs-target="#exampleModal">+Add New Student</button>
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h1 class="modal-title fs-5" id="exampleModalLabel">Add New Students</h1>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            <form id="form" action="insert.php" method="POST" enctype="multipart/form-data">
+              <input type="hidden" id="id" name="id">
+              <div class="mb-3">
+                <label for="name" class="col-form-label">Username:</label>
+                <input type="text" class="form-control" id="name" name="name">
+              </div>
+              <div class="mb-3">
+                <label for="gender" class="col-form-label">Gender</label>
+                <select name="gender" id="gender" class="form-select">
+                  <option value="">Select Gender</option>
+                  <option value="male">Male</option>
+                  <option value="female">Female</option>
+                </select>
+              </div>
+              <div>
+                <label for="profile" class="col-form-label">Profile</label>
+                <img src="https://i.pinimg.com/736x/9d/16/4e/9d164e4e074d11ce4de0a508914537a8.jpg" alt="" width="110px" height="110px" id="image" class="m-2">
+                <input type="file" class="form-control" id="file" name="file">
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" id="update" name="update" class="btn btn-warning">Update</button>
+                <button type="button" id="save" name="submit" data-bs-dismiss="modal" class="btn btn-primary">Save</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
     <table class="table table-hover text-center">
       <thead>
         <tr>
@@ -23,42 +60,8 @@
           <th>Actions</th>
         </tr>
       </thead>
+
       <tbody>
-        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-          <div class="modal-dialog">
-            <div class="modal-content">
-              <div class="modal-header">
-                <h1 class="modal-title fs-5" id="exampleModalLabel">Add New Students</h1>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-              </div>
-              <div class="modal-body">
-                <form id="form" action="insert.php" method="POST" enctype="multipart/form-data">
-                  <input type="hidden" id="id" name="id">
-                  <div class="mb-3">
-                    <label for="recipient-name" class="col-form-label">Username:</label>
-                    <input type="text" class="form-control" id="name" name="name">
-                  </div>
-                  <div class="mb-3">
-                    <label for="message-text" class="col-form-label">Gender</label>
-                    <select name="gender" id="gender" class="form-select">
-                      <option value="">Select Gender</option>
-                      <option value="male">Male</option>
-                      <option value="female">Female</option>
-                    </select>
-                  </div>
-                  <div>
-                    <label for="profile" class="col-form-label">Profile</label>
-                    <input type="file" class="form-control" id="file" name="file">
-                  </div>
-                  <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                <button type="submit" id="save" name="submit" class="btn btn-primary">Save</button>
-              </div>
-                </form>
-              </div>
-            </div>
-          </div>
-        </div>
         <?php
         include 'db.php';
         $select = "SELECT * FROM tbl_student";
@@ -70,7 +73,7 @@
           <td>' . $row['name'] . '</td>
           <td>' . $row['gender'] . '</td>
           <td>
-              <img src="' . $row['profile'] . '" alt="" width="50px" height="50px" class="">
+              <img src="' . $row['profile'] . '" alt="" width="50px">
           </td>
           <td>
               <div class="d-flex justify-content-center gap-2">
@@ -89,5 +92,67 @@
     </table>
   </div>
 </body>
+<script>
+  $(document).ready(function() {
+    // $('#file').hide()
+    // $('#image').click(function() {
+    //   $('#file').click()
+    // })
+    // $('#file').change(function() {
+    //   let file = this.files[0]
+    //   if (file) {
+    //     let image = URL.createObjectURL(file)
+    //     $('#image').attr('src', image)
+    //   }
+    // })
+    // $('#add').click(function() {
+    //   $('#save').show()
+    //   $('#update').hide()
+    //   $('#exampleModalLabel').text('Add New Students')
+    //   $('#form').attr('action', 'insert.php')
+    //   $('#form').trigger('reset')
+    // })
+
+    $('#save').click(function() {
+      const name = $('#name').val()
+      const gender = $('#gender').val()
+      const profile = $('#file')[0].files[0];
+
+      let formdata= new FormData()
+      formdata.append('file', profile)
+      formdata.append('name', name)
+      formdata.append('gender', gender)
+      $.ajax({
+        url: 'insert.php',
+        method: 'POST',
+        data: formdata,
+        contentType: false,
+        processData: false,
+        success: function(res) {
+          $('#form').trigger('reset');
+          alert(res)
+        }
+      })
+    })
+
+    // $(document).on('click', '#edit', function() {
+    //   $('#save').hide()
+    //   $('#update').show()
+    //   $('#exampleModalLabel').text('Edit Students')
+    //   $('#form').attr('action', 'update.php')
+
+    //   const row = $(this).closest('tr');
+    //   const id = row.find('td:eq(0)').text().trim()
+    //   const name = row.find('td:eq(1)').text().trim()
+    //   const gender = row.find('td:eq(2)').text().trim()
+    //   $('#image').attr('src', row.find('img').attr('src'));
+
+    //   $('#id').val(id)
+    //   $('#name').val(name)
+    //   $('#gender').val(gender)
+    //   $('#profile').attr('src', profile)
+    // })
+  })
+</script>
 
 </html>
